aa0bf274072aa5845221c9df9430d3986e35e695
ivantarabanovych <vanjokcitet334@gmail.com>
2024-10-29 01:04:49 +0200

    q3

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-const _ = require('lodash');
+function add(a, b) {
+    return a + b;
+}
 
-const numbers = [10, 45, 63, 
-                  9, 2, 75];
-
-const maxSum = _.sum(_.slice(_.sortBy(numbers), -2)); 
-
-console.log(`Сума двох найбільших чисел: ${maxSum}`);
+module.exports = add;
